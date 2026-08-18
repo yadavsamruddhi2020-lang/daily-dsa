d0bcc569a9ab3b6b0a41d0dda3d3d1a1d92c2533
Samruddhi <yadavsamruddhi2020@gamil.com>
2026-08-18 22:22:19 +0530

    Add daily array DSA programs

---
+++ array/day2.java
@@ -0,0 +1,16 @@
+public class day2 {
+    public static void main(String[] args){
+        int arr[]={1,2,34,54,66};
+        int evenCount=0;
+        int oddCount=0;
+        for(int i=0;i< arr.length;i++){
+            if(arr[i]%2==0){
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+        System.out.println("Even numbers count: " + evenCount);
+        System.out.println("Odd numbers count: " + oddCount);
+    }
+}
